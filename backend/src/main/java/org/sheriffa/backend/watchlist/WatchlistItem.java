570354431cd241fb5317315ec366c81a678fc2c4
Sheriff-A <manshari619@yahoo.com>
2026-09-10 22:36:40 -0400

    watchlist and item db data

--- backend/src/main/java/org/sheriffa/backend/watchlist/WatchlistItem.java
+++ backend/src/main/java/org/sheriffa/backend/watchlist/WatchlistItem.java
@@ -1,4 +1,22 @@
 package org.sheriffa.backend.watchlist;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class WatchlistItem {
+    private UUID id;
+    private UUID watchlist_id;
+    private String media_id;
+    private WatchlistItemMediaType media_type;
+    private int position;
+    private Object metadata;
+    private OffsetDateTime created_at;
+    private OffsetDateTime updated_at;
 }
--- backend/src/main/java/org/sheriffa/backend/watchlist/WatchlistItemMediaType.java
+++ backend/src/main/java/org/sheriffa/backend/watchlist/WatchlistItemMediaType.java
@@ -1,4 +1,5 @@
 package org.sheriffa.backend.watchlist;
 
 public enum WatchlistItemMediaType {
+    MOVIE, TV_SHOW
 }
