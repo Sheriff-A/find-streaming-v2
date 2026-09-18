f6b5e3707f8d618b4826b1040767df24de63ff3d
Sheriff-A <manshari619@yahoo.com>
2026-09-17 21:55:48 -0400

    filled in watchlist item response dto

--- backend/src/main/java/org/sheriffa/backend/watchlist/dto/GetWatchlistItemResponse.java
+++ backend/src/main/java/org/sheriffa/backend/watchlist/dto/GetWatchlistItemResponse.java
@@ -1,4 +1,31 @@
 package org.sheriffa.backend.watchlist.dto;
 
-public record GetWatchlistItemResponse() {
+import org.sheriffa.backend.watchlist.WatchlistItem;
+import org.sheriffa.backend.watchlist.WatchlistItemMediaType;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+public record GetWatchlistItemResponse(
+        UUID id,
+        UUID watchlist_id,
+        String media_id,
+        WatchlistItemMediaType media_type,
+        int position,
+        String metadata,
+        OffsetDateTime created_at,
+        OffsetDateTime updated_at
+) {
+    public static GetWatchlistItemResponse from(WatchlistItem watchlistItem) {
+        return new GetWatchlistItemResponse(
+                watchlistItem.getId(),
+                watchlistItem.getWatchlist_id(),
+                watchlistItem.getMedia_id(),
+                watchlistItem.getMedia_type(),
+                watchlistItem.getPosition(),
+                watchlistItem.getMetadata(),
+                watchlistItem.getCreated_at(),
+                watchlistItem.getUpdated_at()
+        );
+    }
 }
