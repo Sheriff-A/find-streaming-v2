ce6e986250bb85fc38ab7e6d1a5030f4c89367b5
Sheriff-A <manshari619@yahoo.com>
2026-09-18 23:41:38 -0400

    variable rename and implemented create watchlist item service function

--- backend/src/main/java/org/sheriffa/backend/watchlist/WatchlistService.java
+++ backend/src/main/java/org/sheriffa/backend/watchlist/WatchlistService.java
@@ -1,6 +1,5 @@
 package org.sheriffa.backend.watchlist;
 
-import org.sheriffa.backend.common.ForbiddenException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,9 +17,9 @@ public class WatchlistService {
     }
 
     // Check if the watchlist exists, and if not, throw a NotFoundException
-    private Watchlist optionalGetWatchList(UUID id) {
-        return watchlistRepository.findById(id)
-                .orElseThrow(() -> new WatchlistNotFoundException(id));
+    private Watchlist optionalGetWatchList(UUID watchlistId) {
+        return watchlistRepository.findById(watchlistId)
+                .orElseThrow(() -> new WatchlistNotFoundException(watchlistId));
     }
 
     private boolean isWatchlistOwner(Watchlist watchlist, UUID requesterId) {
@@ -49,60 +48,75 @@ public class WatchlistService {
         return watchlistRepository.insert(toCreate);
     }
 
-    public Watchlist updateWatchlist(UUID id, UUID requesterId, String name, String description, WatchlistVisibility visibility) {
-        Watchlist watchlist = optionalGetWatchList(id);
+    public Watchlist updateWatchlist(UUID watchlistId, UUID requesterId, String name, String description, WatchlistVisibility visibility) {
+        Watchlist watchlist = optionalGetWatchList(watchlistId);
 
         if (!isWatchlistOwner(watchlist, requesterId)) {
             // A non-owner asking to update a watchlist
             // Forbidden.
             // Cannot update a watchlist that's not yours.'
-            throw new WatchlistForbiddenException(id);
+            throw new WatchlistForbiddenException(watchlistId);
         }
-        Watchlist toUpdate = new Watchlist(id, requesterId, name, description, visibility, null, null);
-        return watchlistRepository.update(id, toUpdate).orElseThrow(() -> new WatchlistNotFoundException(id));
+        Watchlist toUpdate = new Watchlist(watchlistId, requesterId, name, description, visibility, null, null);
+        return watchlistRepository.update(watchlistId, toUpdate).orElseThrow(() -> new WatchlistNotFoundException(watchlistId));
     }
 
-    public void deleteWatchlist(UUID id, UUID requesterId) {
-        Watchlist watchlist = optionalGetWatchList(id);
+    public void deleteWatchlist(UUID watchlistId, UUID requesterId) {
+        Watchlist watchlist = optionalGetWatchList(watchlistId);
 
         if (!isWatchlistOwner(watchlist, requesterId)) {
             // A non-owner asking to delete a watchlist
             // Forbidden.
             // Cannot delete a watchlist that's not yours.'
-            throw new WatchlistForbiddenException(id);
+            throw new WatchlistForbiddenException(watchlistId);
         }
-        watchlistRepository.deleteById(id);
+        watchlistRepository.deleteById(watchlistId);
     }
 
-    public Watchlist patchWatchlist(UUID id, UUID requesterId, String name, String description, WatchlistVisibility visibility) {
-        Watchlist watchlist = optionalGetWatchList(id);
+    public Watchlist patchWatchlist(UUID watchlistId, UUID requesterId, String name, String description, WatchlistVisibility visibility) {
+        Watchlist watchlist = optionalGetWatchList(watchlistId);
 
         if (!isWatchlistOwner(watchlist, requesterId)) {
             // A non-owner asking to patch a watchlist
             // Forbidden.
             // Cannot patch a watchlist that's not yours.'
-            throw new WatchlistForbiddenException(id);
+            throw new WatchlistForbiddenException(watchlistId);
         }
 
         String updatedName = name != null ? name : watchlist.getName();
         String updatedDescription = description != null ? description : watchlist.getDescription();
         WatchlistVisibility updatedVisibility = visibility != null ? visibility : watchlist.getVisibility();
 
-        Watchlist toPatch = new Watchlist(id, requesterId, updatedName, updatedDescription, updatedVisibility, null, null);
-        return watchlistRepository.update(id, toPatch).orElseThrow(() -> new WatchlistNotFoundException(id));
+        Watchlist toPatch = new Watchlist(watchlistId, requesterId, updatedName, updatedDescription, updatedVisibility, null, null);
+        return watchlistRepository.update(watchlistId, toPatch).orElseThrow(() -> new WatchlistNotFoundException(watchlistId));
     }
 
-    public List<WatchlistItem> getWatchlistItemsInWatchlist(UUID id, UUID requesterId) {
-        Watchlist watchlist = optionalGetWatchList(id);
+    public List<WatchlistItem> getWatchlistItems(UUID watchlistId, UUID requesterId) {
+        Watchlist watchlist = optionalGetWatchList(watchlistId);
 
         if (!isWatchlistOwner(watchlist, requesterId)) {
             // A non-owner asking to get items in a watchlist
             // Forbidden.
             // Cannot get items in a watchlist that's not yours.'
-            throw new WatchlistForbiddenException(id);
+            throw new WatchlistForbiddenException(watchlistId);
         }
 
-        return watchlistItemRepository.findAllByWatchlistId(id);
+        return watchlistItemRepository.findAllByWatchlistId(watchlistId);
 
+    }
+
+    public WatchlistItem createWatchlistItem(UUID watchlistId, UUID requesterId, String mediaId, WatchlistItemMediaType mediaType, String metadata) {
+        Watchlist watchlist = optionalGetWatchList(watchlistId);
+
+        if (!isWatchlistOwner(watchlist, requesterId)) {
+            // A non-owner asking to create an item in a watchlist
+            // Forbidden.
+            // Cannot create an item in a watchlist that's not yours.'
+            throw new WatchlistForbiddenException(watchlistId);
+        }
+
+        int position = watchlistItemRepository.nextPositionFor(watchlistId);
+        WatchlistItem toCreate = new WatchlistItem(null, watchlistId, mediaId, mediaType, position, metadata, null, null);
+        return watchlistItemRepository.insert(toCreate);
     }
 }
